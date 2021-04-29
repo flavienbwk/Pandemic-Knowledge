73c9146abe01365e2d2a2be14f3b04dd296f2c8c
flavienbwk <flavien@berwick.fr>
2021-04-29 18:42:49 +0200

    Updated news webapp

--- searchkit/app/app/src/Home.js
+++ searchkit/app/app/src/Home.js
@@ -1,33 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import styled from 'styled-components';
-import SearchUI from './SearchUI'
+import SearchUI from './SearchUI';
 
 const Styles = styled.div`
-    .paddind-bottom {
-        padding-bottom: 16px;
-    }
-`
+	.paddind-bottom {
+		padding-bottom: 16px;
+	}
+`;
 
 class Home extends Component {
+	render() {
+		return (
+			<Styles>
+				<Container fluid>
+					<Row className="paddind-bottom">
+						<Col lg={{ span: 12 }}>
+							<SearchUI />
+						</Col>
+					</Row>
+				</Container>
+			</Styles>
+		);
+	}
 
-    render() {
-        return (
-            <Styles>
-                <Container fluid>
-                    <Row className="paddind-bottom">
-                        <Col lg={{ span: 12 }}>
-                            <SearchUI/>
-                        </Col>
-                    </Row>
-                </Container>
-            </Styles>
-        )
-    }
-
-    componentDidMount() {
-        document.title = "Home - Pandemic Knowledge";
-    }
+	componentDidMount() {
+		document.title = 'Search - Pandemic Knowledge';
+	}
 }
 
 export default Home;
--- searchkit/app/app/src/SearchUI.js
+++ searchkit/app/app/src/SearchUI.js
@@ -1,114 +1,106 @@
-import React, { Component } from 'react'
-import { Col } from 'react-bootstrap';
-import { SearchkitManager, SearchkitProvider, SearchBox, Hits } from "searchkit";
-import Highlighter from "react-highlight-words";
+import React, { Component } from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
+import { SearchkitManager, SearchkitProvider, SearchBox, Hits } from 'searchkit';
+import Highlighter from 'react-highlight-words';
 
-const sk = new SearchkitManager("https://172.17.0.1:9200/kibana_sample_data_ecommerce/", {
-  basicAuth:"elastic:elastic",
-})
-
-class CustomHits extends Component {
-
-    constructor(props) {
-      super(props);
-    }
-
-    render() {
-      // console.log(this.props.result._source)
-        return  <Col lg={{ span: 12 }}>
-                  <div style={{border: '2px solid #d1d1d1', marginTop:20, height: 120, backgroundColor: "white", borderRadius: 5, display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                    <div style={{ marginLeft: 16, marginRight: 16 }}>
-                      <img style={{height: 70, width: 70, borderRadius: 35}} alt="" src={"https://static.remove.bg/remove-bg-web/2a274ebbb5879d870a69caae33d94388a88e0e35/assets/start-0e837dcc57769db2306d8d659f53555feb500b3c5d456879b9c843d1872e7baa.jpg"} />
-                    </div>
-                    <div>
-                      <div style={{fontWeight:"bold", fontSize:20}}>
-                        <a href="www.google.fr">
-                          <Highlighter
-                            searchWords={[this.props.queryValue]}
-                            textToHighlight={this.props.result._source.customer_first_name}
-                            highlightStyle={{backgroundColor: "#fcf403"}}
-                          />
-                        </a>
-                      </div>
-                      <div>
-                        <Highlighter
-                          searchWords={[this.props.queryValue]}
-                          textToHighlight={this.props.result._source.email}
-                          highlightStyle={{backgroundColor: "#fcf403"}}
-                        />
-                      </div>
-                      <div style={{fontSize:10}}>
-                        source:
-                      </div>
-                      <div style={{fontSize:10}}>
-                        sentiment:
-                      </div>
-                      <div style={{fontSize:10, marginBottom:5}}>
-                        {this.props.result._source.email}
-                      </div>
-                    </div>
-                  </div>
-                </Col>
-    }
-}
+const search_kit = new SearchkitManager('https://172.17.0.1:9200/news_*/', {
+	basicAuth: 'elastic:elastic'
+});
 
 export class SearchUI extends Component {
+	state = {
+		queryValue: ''
+	};
 
-    state = {
-      queryValue: ""
-    }
+	queryBuilder = (queryString) => {
+		this.setState({ queryValue: queryString });
+		return {
+			bool: {
+				must: [],
+				filter: [
+					{
+						multi_match: {
+							type: 'best_fields',
+							query: queryString,
+							lenient: true
+						}
+					}
+				],
+				should: [],
+				must_not: []
+			}
+		};
+	};
 
-    queryBuilder = (queryString) => {
-        this.setState({queryValue: queryString})
-        return {
-            "bool": {
-                "must": [],
-                "filter": [
-                  {
-                    "multi_match": {
-                      "type": "best_fields",
-                      "query": queryString,
-                      "lenient": true,
-                    }
-                  },
-                  {
-                    "range": {
-                      "order_date": {
-                        "gte": "2016-04-28T13:59:33.997Z",
-                        "lte": "2021-07-28T15:00:28.904Z",
-                        "format": "strict_date_optional_time"
-                      }
-                    }
-                  }
-                ],
-                "should": [],
-                "must_not": [],
-            }
-        }
-    }
-    
-    render() {
-        return (
-          <div style={{display: "flex", flex:1}}>
-            <SearchkitProvider searchkit={sk}>
-              <div style={{alignItems: "center", display: "flex", width: "100%", marginBottom: 50, flexDirection:"column", marginTop: 50, backgroundColor: '#e6e6e6'}}>
-                <div style={{fontSize:30, marginTop: 20}}>
-                  Enter any keyword
-                </div>
-                <div style={{marginTop:15}}>
-                  <SearchBox
-                    searchOnChange={true}
-                    queryBuilder={(queryString) => this.queryBuilder(queryString)}
-                  />
-                </div>
-                <div style={{justifyContent: "center", alignItems: "center", width:"80%", marginTop:50}}>
-                  <Hits itemComponent={<CustomHits queryValue={this.state.queryValue}/>}/>
-                </div>
-              </div>
-            </SearchkitProvider>
-          </div>
-        );
-    }
+	render() {
+		return (
+			<SearchkitProvider searchkit={search_kit}>
+				<div>
+					<Row>
+						<Col lg={{ span: 12 }} className="pt-4 pb-4">
+							<SearchBox
+								translations={{ 'searchbox.placeholder': 'Search COVID-related news' }}
+								autofocus={true}
+								className={'form-control'}
+								searchOnChange={true}
+								queryBuilder={this.queryBuilder}
+							/>
+						</Col>
+					</Row>
+					<Row>
+						<Hits hitsPerPage={8} itemComponent={<News queryValue={this.state.queryValue} />} />
+					</Row>
+				</div>
+			</SearchkitProvider>
+		);
+	}
+}
+
+class News extends Component {
+	render() {
+		return (
+			<Row className="mt-2"
+				onClick={() => {
+					window.open(this.props.result._source.link)
+				}}
+				style={{
+					cursor: "pointer"
+				}}
+				title={this.props.result._source.link}
+			>
+				<Col lg={{ span: 2 }}>{<img style={{ width: '100%' }} src={this.props.result._source.img} />}</Col>
+				<Col lg={{ span: 8 }}>
+					<Card>
+						<Card.Body>
+							<Card.Title>
+								<Highlighter
+									searchWords={[this.props.queryValue]}
+									textToHighlight={this.props.result._source.title}
+									highlightStyle={{ backgroundColor: '#fcf403' }}
+								/>
+								<br />
+								{
+									(this.props.result._source.date)
+										?
+										<small>
+											<b>{new Date(this.props.result._source.date).toLocaleDateString('fr-FR')}</b>
+										</small>
+										: <></>
+								}
+							</Card.Title>
+							<Card.Text>
+								<Highlighter
+									searchWords={[this.props.queryValue]}
+									textToHighlight={this.props.result._source.desc}
+									highlightStyle={{ backgroundColor: '#fcf403' }}
+								/>
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		);
+	}
 }
 
 export default SearchUI;
